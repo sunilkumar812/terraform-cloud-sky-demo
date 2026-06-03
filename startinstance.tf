970436ee1b8d304550c6451c0e5dbbf8d40e2c32
sunilkumar812 <57595813+sunilkumar812@users.noreply.github.com>
2026-06-03 10:41:40 +0530

    Comment out EC2 instance start resource
    Comment out the resource block for starting an EC2 instance.

--- startinstance.tf
+++ startinstance.tf
@@ -1,4 +1,4 @@
-resource "aws_ec2_instance_state" "start_instance" {
-  instance_id = aws_instance.first-tf-web-server.id
-  state       = "running"
-}
+# resource "aws_ec2_instance_state" "start_instance" {
+#  instance_id = aws_instance.first-tf-web-server.id
+#  state       = "running"
+# }
