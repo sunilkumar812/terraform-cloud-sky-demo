2d29128ee5e5d71d4a7d7a5596abb45bb86a556a
sunilkumar812 <57595813+sunilkumar812@users.noreply.github.com>
2026-06-03 10:42:05 +0530

    Comment out stop_instance resource in stopinstance.tf
    Comment out the resource block for stopping an EC2 instance.

--- stopinstance.tf
+++ stopinstance.tf
@@ -1,4 +1,4 @@
-resource "aws_ec2_instance_state" "stop_instance" {
-  instance_id = aws_instance.first-tf-web-server.id
-  state       = "stopped"
-}
+# resource "aws_ec2_instance_state" "stop_instance" {
+#  instance_id = aws_instance.first-tf-web-server.id
+#  state       = "stopped"
+#}
