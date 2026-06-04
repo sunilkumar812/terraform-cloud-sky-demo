3c443e103ad5b18ea872097fe019c51c36642f9e
sunilkumar812 <57595813+sunilkumar812@users.noreply.github.com>
2026-06-04 10:49:44 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,4 +1,4 @@
 # Create S3 bucket
-resource "aws_s3_bucket" "public_bucket" {
-  bucket = "sktest-my-public-bucket-12345"
-}
+#resource "aws_s3_bucket" "public_bucket" {
+#  bucket = "sktest-my-public-bucket-12345"
+#}
