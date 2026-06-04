f59bcc3e07cd68d6d4f1f357f0c0d054dceae0a4
sunilkumar812 <57595813+sunilkumar812@users.noreply.github.com>
2026-06-04 18:55:08 +0530

    Update lambda_function.py

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,11 +1,59 @@
 import json
+import boto3
+
+lambda_client = boto3.client("lambda")
+
+REQUIRED_TAGS = [
+    "ApplicationName",
+    "TechnicalOwner",
+    "BusinessOwner"
+]
+
+
+def get_tag(tags, tag_name):
+    return tags.get(tag_name, "Tag does not exist")
+
 
 def lambda_handler(event, context):
 
-    print("===== AWS HEALTH EVENT RECEIVED =====")
+    print("Received Event:")
     print(json.dumps(event, indent=2))
 
-    return {
-        "statusCode": 200,
-        "body": "Event logged successfully"
-    }
+    lambda_arn = event.get("lambdaArn")
+
+    if not lambda_arn:
+        return {
+            "statusCode": 400,
+            "message": "lambdaArn not provided"
+        }
+
+    try:
+
+        response = lambda_client.list_tags(
+            Resource=lambda_arn
+        )
+
+        tags = response.get("Tags", {})
+
+        result = {
+            "LambdaArn": lambda_arn,
+            "ApplicationName": get_tag(tags, "ApplicationName"),
+            "TechnicalOwner": get_tag(tags, "TechnicalOwner"),
+            "BusinessOwner": get_tag(tags, "BusinessOwner")
+        }
+
+        print(json.dumps(result, indent=2))
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps(result)
+        }
+
+    except Exception as e:
+
+        print(f"Error: {str(e)}")
+
+        return {
+            "statusCode": 500,
+            "message": str(e)
+        }
